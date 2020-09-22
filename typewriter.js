c526ab20feb9dbef9e4ffcbf1835a716398ecf13
JoshGrant5 <joshgg@icloud.com>
2020-09-22 03:39:33 +0000

    Playing around with setTimeout and recursion

---
+++ typewriter.js
@@ -0,0 +1,20 @@
+const sentence = "hello there from lighthouse labs";
+
+const delay = sentence => {
+  let timePer = 50; 
+  let index = 0;
+  const printLetter = nextLetter => { 
+    if (index < sentence.length) {
+      process.stdout.write(sentence[index]); 
+    } else {
+      console.log('\n');
+      return null; // is there a better way to break the loop?
+    }
+    index++;
+    timePer += 10;
+    setTimeout(printLetter, timePer);
+  }
+  printLetter();
+}
+
+delay(sentence);
